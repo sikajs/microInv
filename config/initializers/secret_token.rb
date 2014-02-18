853bdf584461e0f54a6f4b371d671b1d1b9d9f5e
Jason Shen <sikajs@gmail.com>
2014-02-18 11:14:32 +0700

    Initialize repository
    use the old database to port the system to ROR
    removed suppl_status field in DB

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmeInv::Application.config.secret_key_base = 'f9dee855ccf5f03b94d7e7934effafc5b701937d3767e7e51434e3eccceed482cac3faf09cf25cbd23432dad6fb18bb077b6a5f094e0f39c355ccf0fa59c7f28'
