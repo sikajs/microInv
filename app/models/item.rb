82a0b8c59867b399af9e5e68144bbda6cd9b276f
Jason Shen <sikajs@gmail.com>
2014-02-21 16:48:29 +0700

    add association between supplier and item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,12 @@
 class Item < ActiveRecord::Base
   self.primary_key = "item_id"
+  self.per_page = 20
+  
   validates :stock, presence: true
   validates :item_name, presence: true
   VALID_BARCODE_REGEX = /[0-9]{9}/
   validates :barcode, presence: true, length: { maximum: 9 },
                       format: { with: VALID_BARCODE_REGEX }
-  self.per_page = 20
+  
+  belongs_to :supplier
 end
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,7 +1,9 @@
 class Supplier < ActiveRecord::Base
   self.primary_key = "supplier_id"
+  self.per_page = 20
   
   validates :suppl_name, presence: true
   validates :business_num, presence: true
-  self.per_page = 20
+  
+  has_many :items
 end
